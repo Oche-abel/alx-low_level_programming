79412beb1f804a24aa322642e004a08e550123ca
oche-abel <ocheabel28@gmail.com>
2023-02-22 06:35:57 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _islower - checks for lowercase characters
- * @C: The character to be checked
+ * @c: The character to be checked
  * Return: 1 for  lowercase characters or 0 for anything else
 */
 
