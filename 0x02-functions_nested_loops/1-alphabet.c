d6ee7d85560f0774a2b0a07e7d7a2d14f9ae8d80
oche-abel <ocheabel28@gmail.com>
2023-02-21 23:01:08 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,14 @@
  * Prints alphabet in lowercase
  * Return: void
 */
+
 void print_alphabet(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		_putchar(c);
+	_putchar(c);
 	}
 	_putchar('\n');
 }
